46d9bed9689d46c305f551d32fdd2163f766928e
daltondhcp <johan@dahlbom.eu>
2019-07-09 09:48:04 +0200

    Create enableakspreviews.sh

---
+++ enableakspreviews.sh
@@ -0,0 +1,12 @@
+#Install aks-preview extension
+az extension add --name aks-preview
+
+#Add preview features to subscription
+az feature register -n WindowsPreview --namespace Microsoft.ContainerService
+az feature register -n AKSLockingDownEgressPreview --namespace Microsoft.ContainerServie
+az feature register -n MultiAgentpoolPreview --namespace Microsoft.ContainerService
+az feature register -n APIServerSecurityPreview --namespace Microsoft.ContainerService
+az feature register -n VMSSPreview --namespace Microsoft.ContainerService
+az feature register -n AKSAuditLog --namespace Microsoft.ContainerService
+az feature register -n PodSecurityPolicyPreview --namespace Microsoft.ContainerService
+az feature register -n AKSAzureStandardLoadBalancer --namespace "Microsoft.ContainerService" 
